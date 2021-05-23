baeaae22b38d2cb503fcced9d39395a8cf770f64
Dani El-Ayyass <dayyass@yandex.ru>
2021-05-23 18:41:53 +0300

    add docstring

--- distribution/abstract.py
+++ distribution/abstract.py
@@ -6,6 +6,16 @@ import numpy as np
 
 # TODO: add method "predict_log_proba"
 class AbstractDistribution(ABC):
+    """
+    Abstract base class to represent probability distributions.
+    """
+
     @abstractmethod
-    def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None):
+    def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> None:
+        """
+        Method to compute MLE given X (data). If y is provided, computes MLE of X for each class y.
+
+        :param np.ndarray X: training data.
+        :param Optional[np.ndarray] y: target values.
+        """
         pass
