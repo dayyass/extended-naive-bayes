78e5985543ae19ba919e24f0e8e0dd0fc983fb69
Dani El-Ayyass <dayyass@yandex.ru>
2021-05-24 23:31:08 +0300

    update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,12 +3,64 @@ import unittest
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, _BaseNB
 
 from distributions import Normal
 from models import GaussianNaiveBayes, NaiveBayes
+from models.abstract import AbstractModel
 
 np.random.seed(42)
+
+
+def compare_model_with_sklean(
+    model: AbstractModel,
+    sklearn_model: _BaseNB,
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    y_train: np.ndarray,
+    y_test: np.ndarray,
+) -> bool:
+    """
+    Function to compare our and sklearn models (all methods).
+
+    :param AbstractModel model: our model.
+    :param _BaseNB sklearn_model: sklearn model.
+    :param np.ndarray X_train: training data.
+    :param np.ndarray X_test: testing data.
+    :param np.ndarray y_train: training target.
+    :param np.ndarray y_test: testing target.
+    :return: True if all equal.
+    :rtype: bool
+    """
+
+    test_1 = np.allclose(
+        model.predict_log_proba(X_train),
+        sklearn_model.predict_log_proba(X_train),
+    )
+    test_2 = np.allclose(
+        model.predict_proba(X_train),
+        sklearn_model.predict_proba(X_train),
+    )
+    test_3 = np.allclose(model.predict(X_train), sklearn_model.predict(X_train))
+    test_4 = np.allclose(
+        model.score(X_train, y_train),
+        sklearn_model.score(X_train, y_train),
+    )
+    test_5 = np.allclose(
+        model.predict_log_proba(X_test),
+        sklearn_model.predict_log_proba(X_test),
+    )
+    test_6 = np.allclose(
+        model.predict_proba(X_test),
+        sklearn_model.predict_proba(X_test),
+    )
+    test_7 = np.allclose(model.predict(X_test), sklearn_model.predict(X_test))
+    test_8 = np.allclose(
+        model.score(X_test, y_test),
+        sklearn_model.score(X_test, y_test),
+    )
+
+    return np.all([test_1, test_2, test_3, test_4, test_5, test_6, test_7, test_8])
 
 
 # TODO: add other sklearn models
@@ -30,48 +82,13 @@ class TestNaiveBayes(unittest.TestCase):
         sklearn_model.fit(self.X_train, self.y_train)
 
         self.assertTrue(
-            np.allclose(
-                model.predict_log_proba(self.X_train),
-                sklearn_model.predict_log_proba(self.X_train),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict_proba(self.X_train),
-                sklearn_model.predict_proba(self.X_train),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict(self.X_train), sklearn_model.predict(self.X_train)
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.score(self.X_train, self.y_train),
-                sklearn_model.score(self.X_train, self.y_train),
-            )
-        )
-
-        self.assertTrue(
-            np.allclose(
-                model.predict_log_proba(self.X_test),
-                sklearn_model.predict_log_proba(self.X_test),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict_proba(self.X_test),
-                sklearn_model.predict_proba(self.X_test),
-            )
-        )
-        self.assertTrue(
-            np.allclose(model.predict(self.X_test), sklearn_model.predict(self.X_test))
-        )
-        self.assertTrue(
-            np.allclose(
-                model.score(self.X_test, self.y_test),
-                sklearn_model.score(self.X_test, self.y_test),
+            compare_model_with_sklean(
+                model,
+                sklearn_model,
+                self.X_train,
+                self.X_test,
+                self.y_train,
+                self.y_test,
             )
         )
 
@@ -94,47 +111,12 @@ class TestGaussianNaiveBayes(unittest.TestCase):
         sklearn_model.fit(self.X_train, self.y_train)
 
         self.assertTrue(
-            np.allclose(
-                model.predict_log_proba(self.X_train),
-                sklearn_model.predict_log_proba(self.X_train),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict_proba(self.X_train),
-                sklearn_model.predict_proba(self.X_train),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict(self.X_train), sklearn_model.predict(self.X_train)
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.score(self.X_train, self.y_train),
-                sklearn_model.score(self.X_train, self.y_train),
-            )
-        )
-
-        self.assertTrue(
-            np.allclose(
-                model.predict_log_proba(self.X_test),
-                sklearn_model.predict_log_proba(self.X_test),
-            )
-        )
-        self.assertTrue(
-            np.allclose(
-                model.predict_proba(self.X_test),
-                sklearn_model.predict_proba(self.X_test),
-            )
-        )
-        self.assertTrue(
-            np.allclose(model.predict(self.X_test), sklearn_model.predict(self.X_test))
-        )
-        self.assertTrue(
-            np.allclose(
-                model.score(self.X_test, self.y_test),
-                sklearn_model.score(self.X_test, self.y_test),
+            compare_model_with_sklean(
+                model,
+                sklearn_model,
+                self.X_train,
+                self.X_test,
+                self.y_train,
+                self.y_test,
             )
         )
